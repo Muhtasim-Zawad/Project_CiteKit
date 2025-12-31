cebf958b99d08dd70e28292801fec79704f4eec9
Muhtasim-Zawad <muhtasimzawad.jfcl@gmail.com>
2025-12-31 13:54:23 +0600

    minor UI modifications in workspace UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import LandingPage from "./pages/landing/LandingPage";
 import Dashboard from "./pages/workspace/Dashboard";
+import Workstation from "./pages/workspace/Workstation";
 
 function App() {
 	return (
 		<div>
 			{/* <LandingPage />  */}
-			<Dashboard />
+			{/* <Dashboard /> */}
+			<Workstation />
 		</div>
 	);
 }
--- frontend/src/pages/workspace/Workstation.jsx
+++ frontend/src/pages/workspace/Workstation.jsx
@@ -6,7 +6,7 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 import data from "./data.json";
 
-export default function Dashboard() {
+export default function Workstation() {
 	return (
 		<SidebarProvider
 			style={{
@@ -20,7 +20,7 @@ export default function Dashboard() {
 				<div className="flex flex-1 flex-col">
 					<div className="@container/main flex flex-1 flex-col gap-2">
 						<div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-							<SectionCards />
+							{/* <SectionCards /> */}
 							{/* <DataTable data={data} /> */}
 						</div>
 					</div>
