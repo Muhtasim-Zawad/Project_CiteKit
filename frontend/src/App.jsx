3d68abbc63ddc6bc415c1ee82b7d2549a92726eb
Muhtasim-Zawad <muhtasimzawad.jfcl@gmail.com>
2025-12-31 12:04:23 +0600

    Configured the initial structure for the dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import { Button } from "@/components/ui/button";
 import LandingPage from "./pages/landing/LandingPage";
+import Dashboard from "./pages/workspace/Dashboard";
 
 function App() {
-	return <div>{/* <LandingPage />  */}</div>;
+	return (
+		<div>
+			{/* <LandingPage />  */}
+			<Dashboard />
+		</div>
+	);
 }
 
 export default App;
