26d22d2feb14ee6f3685e9008889805abe4a8447
Muhtasim-Zawad <muhtasimzawad.jfcl@gmail.com>
2025-12-31 15:58:16 +0600

    added functionalities and styling to the editor menubar in workspace

--- frontend/src/components/rich-text-editor/editor.jsx
+++ frontend/src/components/rich-text-editor/editor.jsx
@@ -3,10 +3,29 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import { FloatingMenu, BubbleMenu } from "@tiptap/react/menus";
 import StarterKit from "@tiptap/starter-kit";
 import MenuBar from "./menu-bar";
+import TextAlign from "@tiptap/extension-text-align";
+import Highlight from "@tiptap/extension-highlight";
 
 export default function Editor() {
 	const editor = useEditor({
-		extensions: [StarterKit], // define your extension array
+		extensions: [
+			StarterKit.configure({
+				bulletList: {
+					HTMLAttributes: {
+						class: "list-disc ml-3",
+					},
+				},
+				orderedList: {
+					HTMLAttributes: {
+						class: "list-decimal ml-3",
+					},
+				},
+			}),
+			TextAlign.configure({
+				types: ["heading", "paragraph"],
+			}),
+			Highlight,
+		], // define your extension array
 		content: "<p>Hello World!</p>",
 		editorProps: {
 			attributes: {
@@ -19,8 +38,8 @@ export default function Editor() {
 		<>
 			<MenuBar editor={editor} />
 			<EditorContent editor={editor} />
-			<FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
-			<BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+			{/* <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+			<BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu> */}
 		</>
 	);
 }
