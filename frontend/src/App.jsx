f26da8a4fdc842e339e91d5e054b34bee003a288
Muhtasim-Zawad <muhtasimzawad.jfcl@gmail.com>
2026-01-24 17:26:48 +0600

    added log in ui in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import LandingPage from "./pages/landing/LandingPage";
 import Dashboard from "./pages/workspace/Dashboard";
 import Workstation from "./pages/workspace/Workstation";
 import Editor from "./components/rich-text-editor/editor";
+import LoginPage from "./components/login";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<LandingPage />} />
+			<Route path="/auth" element={<LoginPage />} />
 			<Route path="/dashboard" element={<Dashboard />} />
 			{/* <Route path="/workspace/:projectId" element={<Workstation />} /> */}
 
